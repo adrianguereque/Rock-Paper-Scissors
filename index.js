3ec68781372fdb8ff8d26e38987350deae3cd9c6
adrian-guereque <124836243+adrianguereque@users.noreply.github.com>
2023-05-11 13:53:27 -0600

    Grouped variables together, added toggleResults, and different counts

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// Variables --------------------------------------
+// buttons
+const Rockbtn = document.querySelector('#rock'); 
+const Paperbtn = document.querySelector('#paper');
+const Scissorsbtn = document.querySelector('#scissors');
+const restartBtn = document.querySelector('#restart');
+// screens
+const gameScreen = document.querySelector('#gameScreen');
+const endScreen = document.querySelector('#endScreen')
+// initial screen values (hidden or not)
+endScreen.style.display = "none";
+gameScreen.style.display = "initial";
+//counts
+computerCount = 0;
+playerCount = 0;
+// How many wins required for winning
+winsToEnd = 5;
+// Result div for text content
+winLose = document.querySelector('.winLose');;
+// -----------------------------------------------------
+
 function getComputerChoice(){
     switch(Math.floor(Math.random() * 3)){
         case 0:
@@ -12,40 +33,56 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     console.log(playerSelection, computerSelection);
     if((playerSelection=="Scissors" && computerSelection=="Paper") || (playerSelection=="Paper" && computerSelection=="Rock") || (playerSelection == "Rock" && computerSelection == "Scissors")){
-        return 1;
+        playerCount += 1;
+        console.log(playerCount , computerCount);
     }
     else if(playerSelection==computerSelection){
-        return 0;
+        return;
     }
     else{
-        return -1;
-    }
-}
-let count = 0;
-const Rockbtn = document.querySelector('#rock');
-Rockbtn.addEventListener('click', () => {
-  count = count + playRound("Rock", getComputerChoice());
-  console.log(count);
-  checkScore();
-});
-const Paperbtn = document.querySelector('#paper');
-Paperbtn.addEventListener('click', () => {
-  count = count + playRound("Paper", getComputerChoice());
-  console.log(count);
-  checkScore();
-});
-const Scissorsbtn = document.querySelector('#scissors');
-Scissorsbtn.addEventListener('click', () => {
-  count = count + playRound("Scissors", getComputerChoice());
-  console.log(count);
-  checkScore();
-});
-function checkScore(){
-    if(count==3){
-        console.log("You win");
-    }
-    if(count==-3){
-        console.log("You lose");
+        computerCount += 1;
+        console.log(playerCount , computerCount);
     }
 }
 
+// Game count and events 
+Rockbtn.addEventListener('click', () => {
+  playRound("Rock", getComputerChoice());
+  checkScore();
+});
+
+Paperbtn.addEventListener('click', () => {
+  playRound("Paper", getComputerChoice());
+  checkScore();
+});
+
+Scissorsbtn.addEventListener('click', () => {
+  playRound("Scissors", getComputerChoice());
+  checkScore();
+});
+
+function checkScore(){ //checks whether someone has won (or lost)
+    if(computerCount == winsToEnd || playerCount == winsToEnd){
+        if(playerCount == winsToEnd){
+            console.log("You win");
+            winLose.textContent = "You win!";
+        }
+        if(computerCount == winsToEnd){
+            console.log("You lose");
+            winLose.textContent = "You lose :("
+        }
+        toggleResult();
+    }
+}
+
+function toggleResult () {
+    gameScreen.style.display = "none";
+    endScreen.style.display = "initial";
+}
+
+restartBtn.addEventListener('click', () => {
+    computerCount = 0;
+    playerCount = 0;
+    gameScreen.style.display = "initial";
+    endScreen.style.display = "none";
+})
